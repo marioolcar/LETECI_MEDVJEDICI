ca8f77953ba2936ff20393cf0721f360bfd1edda
MatijaHudin <matija.hudin@yahoo.com>
2023-11-13 16:24:20 +0100

    bugfix

--- IzvorniKod/SpotPicker/Service/Interface/IKorisnikService.cs
+++ IzvorniKod/SpotPicker/Service/Interface/IKorisnikService.cs
@@ -7,7 +7,7 @@ namespace SpotPicker.Service.Interface
         public Task<List<Korisnik>> GetAllKorisnik();
         public Task<Korisnik?> GetKorisnik(int korisnikId);
         public Task<Korisnik?> Registracija(Korisnik k);
-        public Task<Korisnik?> Enable(int korisnikId);
+        public Task<Korisnik?> ChangeAccountEnabled(int korisnikId);
         public Task<Korisnik?> Login(string? username, string? password, string? confirmpassword);
     }
 }
