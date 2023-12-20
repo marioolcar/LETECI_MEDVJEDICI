3f034ba780de4c34a1ab0f861d3a9071ca14db83
MatijaHudin <matija.hudin@yahoo.com>
2023-12-20 16:19:21 +0100

    ostatak mappera

---
+++ IzvorniKod/SpotPicker/Service/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SpotPicker.Model;
+using SpotPicker.Model.Dtos;
+
+namespace SpotPicker.Service
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<KorisnikDto, Korisnik>()
+                .ForMember(x => x.PictureData, opt => opt.MapFrom(x => (byte[]?) null));
+        }
+    }
+}
