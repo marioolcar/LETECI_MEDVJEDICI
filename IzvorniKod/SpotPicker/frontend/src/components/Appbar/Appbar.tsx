d8dc4de47c37e2e3b14633c96a3e5b0adf07bd13
Paula Močinić <138582037+paulaMocinic@users.noreply.github.com>
2024-01-18 15:29:10 +0100

    Dodan prikaz username-a i obavijest o mailu

--- IzvorniKod/SpotPicker/frontend/src/components/Appbar/Appbar.tsx
+++ IzvorniKod/SpotPicker/frontend/src/components/Appbar/Appbar.tsx
@@ -6,6 +6,7 @@ import {
   IconButton,
   Menu,
   MenuItem,
+  Stack,
   Toolbar,
   Tooltip,
   Typography,
@@ -13,50 +14,64 @@ import {
 import { useState } from "react";
 import React from "react";
 
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const settings = ["Logout"];
+interface AppbarProps {
+  setJwtToken: any;
+}
 
-export function Appbar(): JSX.Element {
+export function Appbar({ setJwtToken }: AppbarProps): JSX.Element {
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
   };
 
-  const handleCloseUserMenu = () => {
+  const handleClick = () => {
+    localStorage.removeItem("jwt-token");
+    setJwtToken(null);
     setAnchorElUser(null);
   };
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+          <Stack
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
+            spacing={2}
+            sx={{ width: "100%" }}
+          >
+            <Stack>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+            </Stack>
+          </Stack>
+
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={() => setAnchorElUser(null)}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleClick}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
         </Toolbar>
       </Container>
     </AppBar>
--- IzvorniKod/SpotPicker/frontend/src/pages/Main/Main.tsx
+++ IzvorniKod/SpotPicker/frontend/src/pages/Main/Main.tsx
@@ -1,22 +1,36 @@
-import { Button } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useUser } from "../../contexts/UserContext";
+import { Appbar } from "../../components/Appbar/Appbar";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface MainProps {
   setJwtToken: any;
 }
 
-export function Main({ setJwtToken }: MainProps) {
+export function Main({ setJwtToken }: MainProps): JSX.Element {
+  const { jwtToken, username } = useUser();
+  console.log("Username from Main component:", username);
+
   const handleClick = () => {
     localStorage.removeItem("jwt-token");
     setJwtToken(null);
   };
   return (
     <>
-      <h1>You are logged in</h1>
-      <Button variant="contained" onClick={handleClick}>
-        Log out
-      </Button>
+      <Appbar setJwtToken={setJwtToken} />
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ marginTop: "2em" }}
+      >
+        <Typography variant="h5" gutterBottom>
+          Dobrodošli {username}!
+        </Typography>
+      </Stack>
     </>
   );
 }
